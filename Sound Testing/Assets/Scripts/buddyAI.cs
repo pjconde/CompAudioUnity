9537a66bf125e44ccd11520d67dc1b439a13b69f
pconde3 <pjconde@gatech.edu>
2017-04-19 20:58:19 -0400

    Added all files needed
    This is the current state of the unity player to use to play the sounds
    for Phase 2 testing

---
+++ Sound Testing/Assets/Scripts/buddyAI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buddyAI : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	public Transform myTransform;
+	public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+	}
+}
